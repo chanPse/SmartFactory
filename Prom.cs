bb0f172d36620317ae811ff549a80aa2d91c48cc
chan <passpsc1@gmail.com>
2018-10-18 12:04:24 +0900

    One

---
+++ Prom.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chanworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("chanworld.exe 세찬 철수 영희 이렇게 넣어주세요.");
+                return;
+            
+            }
+            Console.WriteLine("여러분 안녕하세요, {0}, {1}, {2}", args[0], args[1], args[2]);
+        
+            
+            
+        }
+    }
+}
